eb47d0dcbdb239322b1c7dd661fa23e617bb9674
Sota Naoi <sota.naoi@juge6.jp>
2023-01-24 21:55:14 +0900

    addChanges

--- app/server.go
+++ app/server.go
@@ -8,6 +8,17 @@ import (
 
 const pongResponse = "+PONG\r\n"
 
+func handler(conn net.Conn) {
+	for {
+		buf := make([]byte, 1024)
+
+		if _, err := conn.Read(buf); err != nil {
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
+		conn.Write([]byte(pongResponse))
+	}
+}
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -16,20 +27,13 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
 	for i := 0; ; i++ {
+		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 		defer conn.Close()
-
-		buf := make([]byte, 1024)
-
-		if _, err := conn.Read(buf); err != nil {
-			fmt.Println("error reading from client: ", err.Error())
-			os.Exit(1)
-		}
-		conn.Write([]byte(pongResponse))
+		go handler(conn)
 	}
 }
